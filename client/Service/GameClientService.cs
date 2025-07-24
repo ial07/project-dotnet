d06b62d9a5ea2924a3f2edd3fa23c07f39772217
Azki Fuadi <azkifuadi@jatelindo.co.id>
2025-07-24 23:55:37 +0700

    Implementasi CRUD pada Pages

--- client/Service/GameClientService.cs
+++ client/Service/GameClientService.cs
@@ -11,25 +11,29 @@ public class GameClientService : IGameClientService
 
     public async Task<List<Game>> GetDataAsync()
     {
-        return await _http.GetFromJsonAsync<List<Game>>("/api/Game/GetData");
-    }
-    public Task<List<Game>> CreateAsync()
-    {
-        throw new NotImplementedException();
+        return await _http.GetFromJsonAsync<List<Game>>("/api/Game/GetData");   
     }
 
-    public Task<List<Game>> DeleteAsync()
+    public async Task<Game> GetDataByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await _http.GetFromJsonAsync<Game>($"/api/Game/GetData/{id}");
     }
 
-    public Task<Game> GetDataByIdAsync(int id)
+    public async Task<Game> CreateAsync(Game games)
     {
-        throw new NotImplementedException();
+        var response = await _http.PostAsJsonAsync("/api/Game", games);
+        return await response.Content.ReadFromJsonAsync<Game>();
     }
 
-    public Task<List<Game>> UpdateAsync()
+    public async Task<Game> UpdateAsync(int id, Game game)
     {
-        throw new NotImplementedException();
+        var response = await _http.PutAsJsonAsync($"/api/Game/{id}", game);
+        return await response.Content.ReadFromJsonAsync<Game>();
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var response = await _http.DeleteAsync($"/api/Game/{id}");
+        return response.IsSuccessStatusCode;
     }
 }
